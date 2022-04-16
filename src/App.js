8727c37a417c4f679670aa704490255105d6b3db
codewithshamim <shamimislamonline@gmail.com>
2022-04-16 15:36:51 +0600

    hero section added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./Pages/Shared/Header";
 import Footer from "./Pages/Shared/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>HOme</h1>
+      <div className="banner-section d-flex justify-content-center align-items-center flex-column">
+        <h1>Best food waiting for your belly</h1>
+
+        <span className="search-container">
+          {" "}
+          <input
+            className="search-box"
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search food items"
+          />
+          <button className="search-btn">Search</button>
+        </span>
+      </div>
     </div>
   );
 };
