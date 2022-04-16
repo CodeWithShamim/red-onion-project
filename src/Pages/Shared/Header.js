3e75cddb5c27819f9f3dd0f3e218317652b719aa
codewithshamim <shamimislamonline@gmail.com>
2022-04-16 15:11:45 +0600

    header & footer updated

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import logo from "../../images/logo2.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="container my-4 d-flex justify-content-between align-items-center">
+      <img className="logo" src={logo} alt="logo" />
+      <div>
+        <span className="fw-bold me-4">Login</span>
+        <button className="signup-btn ">Sign up</button>
+      </div>
     </div>
   );
 };
