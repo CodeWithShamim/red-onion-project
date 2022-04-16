3532e3e27b8cf38c997e95f7a3dcc93788bf9976
codewithshamim <shamimislamonline@gmail.com>
2022-04-16 17:32:47 +0600

    home component updated

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="banner-section d-flex justify-content-center align-items-center flex-column">
+        <h2>Best food waiting for your belly</h2>
+
+        <span className="search-container">
+          {" "}
+          <input
+            className="search-box"
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search food items"
+          />
+          <button className="search-btn">Search</button>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,24 +1,10 @@
 import React from "react";
-import "./Home.css";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <div className="banner-section d-flex justify-content-center align-items-center flex-column">
-        <h1>Best food waiting for your belly</h1>
-
-        <span className="search-container">
-          {" "}
-          <input
-            className="search-box"
-            type="search"
-            name="search"
-            id="search"
-            placeholder="Search food items"
-          />
-          <button className="search-btn">Search</button>
-        </span>
-      </div>
+      <Banner></Banner>
     </div>
   );
 };
